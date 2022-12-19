a20122c753173832fdef16fbc05c37cc95d1b4cc
sss <kirillku4mar24@gmail.com>
2022-12-19 15:47:19 +0300

    Initial Commit

---
+++ GuessNumberSwiftUI+UIKIt/GuessNumberSwiftUI_UIKItApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessNumberSwiftUI_UIKItApp.swift
+//  GuessNumberSwiftUI+UIKIt
+//
+//  Created by Supodoco on 19.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessNumberSwiftUI_UIKItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
